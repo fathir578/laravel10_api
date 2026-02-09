39be8ec287a8ac93ca792c639f6444ef1a1dc785
fathir <fathirsyahputra559@gmail.com>
2026-02-09 11:06:56 +0700

    add post resource

--- app/Http/Resources/PostResouce.php
+++ app/Http/Resources/PostResouce.php
@@ -2,18 +2,42 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResouce extends JsonResource
+class PostResource extends JsonResource
 {
+    //define properti
+    public $status;
+    public $message;
+    public $resource;
+    
+    /**
+     * __construct
+     *
+     * @param  mixed $status
+     * @param  mixed $message
+     * @param  mixed $resource
+     * @return void
+     */
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status  = $status;
+        $this->message = $message;
+    }
+
     /**
      * Transform the resource into an array.
      *
-     * @return array<string, mixed>
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
      */
-    public function toArray(Request $request): array
+    public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource
+        ];
     }
 }
